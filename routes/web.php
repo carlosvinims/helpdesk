e2b531eefa3fc590174d5d5af570c1fccb3940ae
Letícia De Paula Lopes <res0147870@aluno.firjansenaisesi.com.br>
2026-09-22 18:57:53 -0300

    tickets atualizada

--- routes/web.php
+++ routes/web.php
@@ -7,29 +7,4 @@ Route::get('/', function () {
     return redirect()->route('tickets.index');
 });
 
-<<<<<<< HEAD
-Route::get('/tickets', [TicketController::class, 'index'])
-    ->name('tickets.index');
-
-Route::get('/tickets/create', [TicketController::class, 'create'])
-    ->name('tickets.create');
-
-Route::post('/tickets', [TicketController::class, 'store'])
-    ->name('tickets.store');
-
-Route::get('/tickets/{ticket}/edit', [TicketController::class, 'edit'])
-    ->name('tickets.edit');
-
-Route::put('/tickets/{ticket}', [TicketController::class, 'update'])
-    ->name('tickets.update');
-
-Route::patch('/tickets/{ticket}/status', [TicketController::class, 'status'])
-    ->name('tickets.status');
-
-Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])
-    ->name('tickets.destroy');
-=======
 Route::resource('tickets', TicketController::class);
-
-    
->>>>>>> d788b7efdef19113aba295f277e4b4d011d824a0
